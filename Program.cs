b8707e33e8f5b1732111e7d9663e7314824ece28
Дмитрий <puhov@tecon.ru>
2024-02-05 11:44:01 +0300

    1

--- Program.cs
+++ Program.cs
@@ -11,37 +11,14 @@ using System.Collections.Generic;
         {
             public static void Main()
             {
-    // using (FileStream fs = new FileStream("user.json", FileMode.OpenOrCreate))
-    // {
-    //     Person tom = new Person("Tom",37, new string[]{"Dad", "Mom", "GrandMom"} );
-    //     JsonSerializer.SerializeAsync<Person>(fs, tom);
-    //     Console.WriteLine("Data has been saved to file");
-    // }
+                 string user = "example.json";
+                 string jsonString = File.ReadAllText(user);
+                 Root? events = JsonSerializer.Deserialize<Root>(jsonString)!;
 
-        string user = "example.json";
-        string jsonString = File.ReadAllText(user);
-        Event events = JsonSerializer.Deserialize<Event>(jsonString)!;
-        Console.WriteLine($"Code: {events?.code} timestamp: {events?.timestamp} payment: {events?.payment}");
-
-
-    // using (FileStream fs = new FileStream("user.json", FileMode.OpenOrCreate))
-    // {
-    //     Person? person = JsonSerializer.DeserializeAsync<Person>(fs);
-    //     Console.WriteLine($"Name: {person?.Name} Age: {person?.Age}");
-    // }
+              for (int i = 0; i < events.events.Count; i++)
+                  Console.WriteLine($"Code: {events?.events[i].code} timestamp: {events?.events[i].timestamp} names: {events?.events[i].names} cheater: {events?.events[i].cheater} payments: {events?.events[i].payments}");
+                    
             }
-    // public class Person
-    // {
-    //     public string? Name { get;}
-    //     public int Age { get; set; }
-    //     public string[] Parents { get; set; }
-
-    //     public Person(string name, int age, string[] parents)
-    //     {
-    //         Name = name;
-    //         Age = age;
-    //         Parents = parents; 
-    //     }
-    }
-        }
+         }
+     }
     
--- events.cs
+++ events.cs
@@ -1,35 +1,13 @@
 using System;
 using System.Text.Json;
 using System.Collections.Generic;
+using System.IO;
 
 namespace serial 
 {
 
-  public class Event
-    {
-        public string code { get; set; }
-        public string step { get; set; }
-        public int timestamp { get; set; }
-        public Payment payment { get; set; }
-        public List<string> names { get; set; }
-        public bool? cheater { get; set; }
-        public List<Payment> payments { get; set; }
-    }
-
-    public class Payment
-    {
-        public string currency { get; set; }
-        public double value { get; set; }
-    }
-
-    public class Payment2
-    {
-        public string currency { get; set; }
-        public int value { get; set; }
-    }
-
     public class Root
     {
-        public List<Event> events { get; set; }
+        public List<Event>? events { get; set; }
     }
 }
